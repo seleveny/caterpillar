2e77e26f32f795d36ab46cf8303c1a595d04a277
xuchengcheng <xuchengcheng@weidian.com>
2019-03-09 15:19:23 +0800

    .

---
+++ caterpillar-gene/src/main/java/com/amc/CaterpillarGeneApplication.java
@@ -0,0 +1,14 @@
+package com.amc;
+
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+
+public class CaterpillarGeneApplication extends SpringBootServletInitializer{
+
+}
