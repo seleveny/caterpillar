c9c243fa8fdfbf6d49b94937c0c4965391f56922
xuchengcheng <xuchengcheng@weidian.com>
2019-03-09 16:42:16 +0800

    ok can start

--- eruca-gene/src/main/java/com/amc/controller/showController.java
+++ eruca-gene/src/main/java/com/amc/controller/showController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@RequestMapping
 public class showController {
 
     @RequestMapping("/show")
--- eruca-gene/src/main/java/com/amc/utils/GenerateGene.java
+++ eruca-gene/src/main/java/com/amc/utils/GenerateGene.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class GenerateGene {
     public static String createImage(String code){
+        //TODO 实现毛毛虫的苹果验证码
         return "";
     }
 }
