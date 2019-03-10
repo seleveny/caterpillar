84e3ee3b2b093e1597a17fd38e07c221d423207a
xuchengcheng <xuchengcheng@weidian.com>
2019-03-10 18:54:56 +0800

    only black

--- eruca-gene/src/main/java/com/amc/utils/GenerateGene.java
+++ eruca-gene/src/main/java/com/amc/utils/GenerateGene.java
@@ -74,47 +74,17 @@ public class GenerateGene {
         Graphics2D g2 = image.createGraphics();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
         Color[] colors = new Color[5];
-        Color[] colorSpaces = new Color[] { Color.WHITE, Color.CYAN,
-                Color.GRAY, Color.LIGHT_GRAY, Color.MAGENTA, Color.ORANGE,
-                Color.PINK, Color.YELLOW };
+        Color[] colorSpaces = new Color[] { Color.BLACK};
         float[] fractions = new float[colors.length];
         for(int i = 0; i < colors.length; i++){
             colors[i] = colorSpaces[rand.nextInt(colorSpaces.length)];
             fractions[i] = rand.nextFloat();
         }
         Arrays.sort(fractions);
-//        g2.setColor(Color.GRAY);// 设置边框色
-//        g2.fillRect(0, 0, w, h);
-        Color c = getRandColor(200, 250);
-        g2.setColor(c);// 设置背景色
+        Color c = Color.BLACK;
+        g2.setColor(Color.WHITE);// 设置背景色
         g2.fillRect(0, 0, w, h);
-        if(level > 2) {
-            //绘制干扰线
-            Random random = new Random();
-            g2.setColor(getRandColor(160, 200));// 设置线条的颜色
-            for (int i = 0; i < 20; i++) {
-                int x = random.nextInt(w - 1);
-                int y = random.nextInt(h - 1);
-                int xl = random.nextInt(6) + 1;
-                int yl = random.nextInt(12) + 1;
-                g2.drawLine(x, y, x + xl + 40, y + yl + 20);
-            }
-        }
-        if(level > 1) {
-            // 添加噪点
-            float yawpRate = 0.05f;// 噪声率
-            int area = (int) (yawpRate * w * h);
-            for (int i = 0; i < area; i++) {
-                int x = random.nextInt(w);
-                int y = random.nextInt(h);
-                int rgb = getRandomIntColor();
-                image.setRGB(x, y, rgb);
-            }
-        }
-        if(level > 0) {
-            shear(g2, w, h, c);// 使图片扭曲
-        }
-        g2.setColor(getRandColor(100, 160));
+        g2.setColor(Color.BLACK);
         int fontSize = h-4;
         Font font = new Font("Algerian", Font.PLAIN, fontSize);
         g2.setFont(font);
@@ -133,84 +103,5 @@ public class GenerateGene {
         ImageIO.write(image, "jpeg", outputStream);
         BASE64Encoder encoder = new BASE64Encoder();
         return encoder.encode(outputStream.toByteArray());
-    }
-    private static Color getRandColor(int fc, int bc) {
-        if (fc > 255)
-            fc = 255;
-        if (bc > 255)
-            bc = 255;
-        int r = fc + random.nextInt(bc - fc);
-        int g = fc + random.nextInt(bc - fc);
-        int b = fc + random.nextInt(bc - fc);
-        return new Color(r, g, b);
-    }
-    private static int getRandomIntColor() {
-        int[] rgb = getRandomRgb();
-        int color = 0;
-        for (int c : rgb) {
-            color = color << 8;
-            color = color | c;
-        }
-        return color;
-    }
-    private static int[] getRandomRgb() {
-        int[] rgb = new int[3];
-        for (int i = 0; i < 3; i++) {
-            rgb[i] = random.nextInt(255);
-        }
-        return rgb;
-    }
-    private static void shear(Graphics g, int w1, int h1, Color color) {
-        shearX(g, w1, h1, color);
-        shearY(g, w1, h1, color);
-    }
-    private static void shearX(Graphics g, int w1, int h1, Color color) {
-        int period = random.nextInt(2);
-        boolean borderGap = true;
-        int frames = 1;
-        int phase = random.nextInt(2);
-        for (int i = 0; i < h1; i++) {
-            double d = (double) (period >> 1)
-                    * Math.sin((double) i / (double) period
-                    + (6.2831853071795862D * (double) phase)
-                    / (double) frames);
-            g.copyArea(0, i, w1, 1, (int) d, 0);
-            if (borderGap) {
-                g.setColor(color);
-                g.drawLine((int) d, i, 0, i);
-                g.drawLine((int) d + w1, i, w1, i);
-            }
-        }
-    }
-    private static void shearY(Graphics g, int w1, int h1, Color color) {
-        int period = random.nextInt(40) + 10; // 50;
-        boolean borderGap = true;
-        int frames = 20;
-        int phase = 7;
-        for (int i = 0; i < w1; i++) {
-            double d = (double) (period >> 1)
-                    * Math.sin((double) i / (double) period
-                    + (6.2831853071795862D * (double) phase)
-                    / (double) frames);
-            g.copyArea(i, 0, 1, h1, 0, (int) d);
-            if (borderGap) {
-                g.setColor(color);
-                g.drawLine(i, (int) d, i, 0);
-                g.drawLine(i, (int) d + h1, i, h1);
-            }
-        }
-    }
-    public static void main(String[] args) {
-        int i=0;
-        while(i++ < 100){
-            Long start = System.currentTimeMillis();
-            try {
-                outputImage(100,32,"ABCD",3);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Long end = System.currentTimeMillis();
-            System.out.println("" + (end-start));
-        }
     }
 }
